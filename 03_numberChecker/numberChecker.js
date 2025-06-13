b0721ad97efec1103ae46723507977ee863d8ab5
xLoopp3r <hipolito.alba.c@gmail.com>
2025-06-13 18:00:40 -0500

    Solución de los ejercicios 3, 6

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,10 +1,17 @@
 function numberChecker(number) {
-  if (number == 10) {
+  if (number >= 10) {
     return true;
   } else {
     return false;
   }
 }
+
+console.log(numberChecker(1000));
+console.log(numberChecker(10));
+console.log(numberChecker(9));
+console.log(numberChecker(6));
+
+
 
 // Do not edit below this line
 module.exports = numberChecker;
--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+    if(count < 0) return "ERROR";
+    let string = "";
+    for(let i=0; i<count; i++) {
+        string = str + string; 
+        console.log(str);
+        
+    }
 
+    return string;
 };
 
+const number = Math.floor(Math.random()*1000);
+repeatString('hey', number);
+
+
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString;    
